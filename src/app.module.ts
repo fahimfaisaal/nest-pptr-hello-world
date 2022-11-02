3c33983d22fa40a5664d3d6d4fbefc284f2db234
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-11-02 10:34:06 +0600

    add async config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PuppeteerModule } from 'nest-puppeteer';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExtractModule } from './extract/extract.module';
 
 @Module({
-  imports: [ExtractModule],
+  imports: [
+    ExtractModule,
+    PuppeteerModule.forRoot({
+      isGlobal: true,
+      pipe: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/extract/extract.module.ts
+++ src/extract/extract.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PuppeteerModule } from 'nest-puppeteer';
 import { ExtractController } from './extract.controller';
 import { ExtractService } from './extract.service';
+import { ConfigModule } from './pptr-config/pptr-config.module';
+import { ConfigService } from './pptr-config/pptrConfig.service';
 
 @Module({
   imports: [
-    PuppeteerModule.forRoot({
-      isGlobal: true,
-      pipe: true,
+    PuppeteerModule.forRootAsync({
+      imports: [ConfigModule],
+      useExisting: ConfigService,
     }),
   ],
   controllers: [ExtractController],
